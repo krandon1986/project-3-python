0f90685bda3d1fc834427e03c49e60d9eced3bdf
krandon1986 <krandon1986@yahoo.co.uk>
2023-03-17 19:24:07 +0000

    Create several empty functions to battleship game before putting in the code

--- run.py
+++ run.py
@@ -1,11 +1,31 @@
 import random
 
-ships_length = [2,3,3,4,5]
-player_board = [[" "] * 8 for i in range(8)]
-computer_board = [[" "] * 8 for i in range(8)]
-guess_board_player = [[" "] * 8 for i in range(8)]
-guess_board_computer = [[" "] * 8 for i in range(8)]
-letters_to_numbers = {'A':0, 'B':1, 'D':3, 'E':4, 'F':5, 'G':6, 'H':7}
+SHIPS_LENGTH = [2,3,3,4,5]
+PLAYER_BOARD = [[" "] * 8 for i in range(8)]
+COMPUTER_BOARD = [[" "] * 8 for i in range(8)]
+GUESS_BOARD_PLAYER = [[" "] * 8 for i in range(8)]
+GUESS_BOARD_COMPUTER = [[" "] * 8 for i in range(8)]
+LETTERS_TO_NUMBERS = {'A':0, 'B':1, 'D':3, 'E':4, 'F':5, 'G':6, 'H':7}
 
+def print_board(board):
+    pass
 
+def ships_placed(board):
+    pass
 
+def check_ships_fit():
+    pass
+
+def overlap_ships():
+    pass
+
+def user_input():
+    pass
+
+def count_hit_ships():
+    pass
+
+def turn(board):
+    pass
+
+while True:
