5472feadfda9a2254b73ece523ffc652693962c6
krandon1986 <krandon1986@yahoo.co.uk>
2023-03-17 19:13:05 +0000

    Create the battleship board for both the player and computer and create a dictionary of converting letters to numbers

--- run.py
+++ run.py
@@ -1,3 +1,11 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import random
+
+ships_length = [2,3,3,4,5]
+player_board = [[" "] * 8 for i in range(8)]
+computer_board = [[" "] * 8 for i in range(8)]
+guess_board_player = [[" "] * 8 for i in range(8)]
+guess_board_computer = [[" "] * 8 for i in range(8)]
+letters_to_numbers = {'A':0, 'B':1, 'D':3, 'E':4, 'F':5, 'G':6, 'H':7}
+
+
+
