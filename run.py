6e96c2094bc12bafd3abd32de1d2299a256896f3
krandon1986 <krandon1986@yahoo.co.uk>
2023-03-17 19:47:25 +0000

    Create a print board template for both the player and the computer board

--- run.py
+++ run.py
@@ -8,7 +8,14 @@ GUESS_BOARD_COMPUTER = [[" "] * 8 for i in range(8)]
 LETTERS_TO_NUMBERS = {'A':0, 'B':1, 'D':3, 'E':4, 'F':5, 'G':6, 'H':7}
 
 def print_board(board):
-    pass
+    print("  A B C D E F G H")
+    print("  +-+-+-+-+-+-+-+")
+    row_number = 1
+    for row in board:
+        print("%d|%s|" % (row_number, "|".join(row)))
+        row_number += 1
+
+print_board(PLAYER_BOARD)
 
 def ships_placed(board):
     pass
@@ -28,4 +35,4 @@ def count_hit_ships():
 def turn(board):
     pass
 
-while True:
+#while True:
