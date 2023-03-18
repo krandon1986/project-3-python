052d8ef2e3c33a51a032e868f6592111ac12b1d0
krandon1986 <krandon1986@yahoo.co.uk>
2023-03-18 03:46:23 +0000

    Create the while loop to run the game, and create a series if-else statements for many different outcomes

--- run.py
+++ run.py
@@ -7,7 +7,7 @@ SHIP_BOARD = [[' '] * 8 for x in range(8)]
 #This is a board displaying the hits and misses. 
 GUESS_BOARD = [[' '] * 8 for x in range(8)]
 
-LETTERS_TO_NUMBERS = {'A':0, 'B':1, 'D':3, 'E':4, 'F':5, 'G':6, 'H':7}
+LETTERS_TO_NUMBERS = {'A':0, 'B':1, 'C':2, 'D':3, 'E':4, 'F':5, 'G':6, 'H':7}
 
 def print_board(board):
     print('  A B C D E F G H')
@@ -26,20 +26,44 @@ def create_battleships(board):
         board[ship_row][ship_column] = 'X'
 
 def get_battleship_location():
-    row = input('Please enter a row number from 1-8 ')
-    while row not in '12345678':
+    row = input('Please enter a row number from 1-8: ')
+    while row not in '1,2,3,4,5,6,7,8':
         print('Please enter a valid row number')
-        row = input('Please enter a row number from 1-8 ')
-    column = input('Please enter a column letter from A-H ').upper()
-    while column not in 'ABCDEFGH':
+        row = input('Please enter a row number from 1-8: ')
+    column = input('Please enter a column letter from A-H: ').upper()
+    while column not in 'A,B,C,D,E,F,G,H':
         print('Please enter a valid column letter')
-        column = input('Please enter a column letter from A-H' ).upper()
+        column = input('Please enter a column letter from A-H: ' ).upper()
     return int(row) - 1, LETTERS_TO_NUMBERS[column]
 
-    def count_hit_battlefield(board):
-        count = 0
-        for row in board:
-            for column in row:
-                if column == 'X':
-                    count += 1
-        return count
+def count_hit_battlefield(board):
+    count = 0
+    for row in board:
+        for column in row:
+            if column == 'X':
+                count += 1
+    return count
+
+create_battleships(SHIP_BOARD)
+turns = 10
+while turns > 0:
+    print('Welcome to Battleship')
+    print_board(GUESS_BOARD)
+    row, column = get_battleship_location()
+    if GUESS_BOARD[row][column] == '/':
+        print('You have aleady selected that area.')
+    elif SHIP_BOARD[row][column] == 'X':
+        print(' Good shot, you have hit the battleship')
+        GUESS_BOARD[row][column] = 'X'
+        turns -= 1
+    else:
+        print('Sorry, you missed your target')
+        GUESS_BOARD[row][column] = '/'
+        turns -= 1
+    if count_hit_battlefield (GUESS_BOARD) == 5:
+        print('Congratulation, you have sunk all the battleships')
+        break
+    print('You have ' + str(turns) + ' turns remaining')
+    if turns == 0:
+        print('Sorry, you have ran out of turns. Game Over!')
+        break
