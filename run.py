f9e4552f56084a0cb379d092a6a6aef8acd63180
krandon1986 <krandon1986@yahoo.co.uk>
2023-03-18 01:51:51 +0000

    Recreate a print board template for the ship and guess board

--- run.py
+++ run.py
@@ -6,3 +6,13 @@
 SHIP_BOARD = [[' '] * 8 for x in range(8)]
 #This is a board displaying the hits and misses. 
 GUESS_BOARD = [[' '] * 8 for x in range(8)]
+
+LETTERS_TO_NUMBERS = {'A':0, 'B':1, 'D':3, 'E':4, 'F':5, 'G':6, 'H':7}
+
+def print_board(board):
+    print('  A B C D E F G H')
+    print('  +-+-+-+-+-+-+-+')
+    row_number = 1
+    for row in board:
+        print(f"%d|%s|" % (row_number, "|".join(row)))
+        row_number += 1
